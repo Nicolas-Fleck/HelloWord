9ed378cc91b729dd10dc3034e3200fa1fa8a5a0b
Nicolas-Fleck <106585400+Nicolas-Fleck@users.noreply.github.com>
2022-05-31 12:29:01 +0200

    Update Hello.cs

--- Hello.cs
+++ Hello.cs
@@ -1,7 +1,9 @@
+using System;
+
 public class Hello
 {
-  public static void main(string[] args)
+  public static void Main(string[] args)
   {
-    Console.Out.WriteLine("Hello, World!");
+    System.Console.Out.WriteLine("Hello, World!");
   }
 }
